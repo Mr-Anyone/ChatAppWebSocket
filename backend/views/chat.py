624b013b12e96ab07dd3b68c8880b317e1a30e42
Mr-Anyone <53135664+Mr-Anyone@users.noreply.github.com>
2023-07-22 15:31:19 +0800

    added api function to get the specifc username

--- backend/views/chat.py
+++ backend/views/chat.py
@@ -8,15 +8,21 @@ id_to_username_table = {}
 
 @socket.on("connect")
 def connect():
-    print(session)
     emit("testing", "this worked! yay!")
     
 
 @socket.on("disconnect")
 def disconnect():
+    # please do some cleanup thank you
     emit("testing", "this is another test I guess")
 
 # this will setthe username
 @socket.on("username")
 def set_username(username):
     id_to_username_table[request.sid] = username
+
+@chat_blueprint("username")
+def get_username():
+    if request.sid in id_to_username_table:
+        return  id_to_username_table[request.sid]
+    return ""
